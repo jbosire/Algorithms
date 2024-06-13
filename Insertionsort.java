c9cb5df984449f15ccbfcf299c27df83061512c6
Joram Bosire <jbosire@vassar.edu>
2024-06-13 01:25:09 -0400

    Update insertionsort

--- Insertionsort.java
+++ Insertionsort.java
@@ -7,7 +7,7 @@ public class Insertionsort {
             int j = i - 1;
 
             //while preceding elements are less than current element
-            while(j > 0 && arr[j] > key){
+            while(j >= 0 && arr[j] > key){
                 //Move item to the next index since key is moving backwards
                 arr[j + 1] = arr[j];
                 //Move one index back
@@ -18,10 +18,23 @@ public class Insertionsort {
         }
     }
 
+    static void reverseSolution(int[] arr){
+        for(int i = 1; i < arr.length; i++){
+            int key = arr[i];
+            int j = i - 1;
+            while(j >= 0 && arr[j] < key){
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+
     public static void main(String[] args){
-        int[] test = {1,3,5,2,3,6,7};
-        solution(test);
-        
+        int[] test = {6,2,3,4,5,10,9,8,7,6,8,3,6,5,1,4,7};
+        //reverseSolution(test);
+        // solution(test);
         for(int i : test){
             System.out.println(i);
         }
